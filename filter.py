24c07a377ee8944593927c8afea98fd9fe4183a8
spacehunterinf <xiaochen.zhu@hhu.de>
2022-10-21 17:31:07 +0100

    finetune scripts

---
+++ filter.py
@@ -0,0 +1,25 @@
+import re
+from argparse import ArgumentParser
+
+def cleaning(s):
+    s = str(s)
+    s = re.sub('\s\W',' ',s)
+    s = re.sub('\W,\s',' ',s)
+    s = re.sub("\d+", "", s)
+    s = re.sub('\s+',' ',s)
+    s = re.sub('[!@#$_]', '', s)
+    s = s.replace("co","")
+    s = s.replace("https","")
+    s = s.replace("[\w*"," ")
+    return s
+
+
+text_data = open('cleaned.txt', 'w', encoding='utf-8')
+
+with open('test_text.txt', 'r', encoding='utf-8') as f:
+    article = f.readlines()
+
+for l in article:
+    text_data.write(cleaning(l))
+
+text_data.close()
